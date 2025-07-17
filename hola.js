6421c684ebd95f7da8e4264b3cb9996730ae6132
netaru3 <netaru3@gmail.com>
2025-07-17 18:18:15 -0300

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,4 +1,6 @@
 let nombre= prompt ("Cual es tu nombre?")
 console.log("hola " + nombre)
 if(nombre!="aaron"&& nombre !="mensom" && nombre!="seba" && nombre!="daniela")
-    {location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+    {let confirmar = confirm("No estás autorizado. ¿Deseas salir?");
+    if (confirmar) {
+        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
