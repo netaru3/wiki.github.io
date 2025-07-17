4029b66a4bf9389a9dd5c64e675748d6fc021615
netaru3 <netaru3@gmail.com>
2025-07-17 18:03:15 -0300

    initial commit

---
+++ hola.js
@@ -0,0 +1,4 @@
+let nombre= prompt ("Cual es tu nombre?")
+console.log("hola " + nombre)
+if(nombre!="aaron"&& nombre !="mensom" && nombre!="seba" && nombre!="daniela")
+    {location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
