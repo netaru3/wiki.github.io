a02c6cfe7173a812880569c931d3ea37494becff
netaru3 <netaru3@gmail.com>
2025-07-17 20:46:03 -0300

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let nombre = prompt("¿Cuál es tu nombre?")}
+ let nombre = prompt("¿Cuál es tu nombre?");
   console.log("Hola " + nombre);
 if(nombre!="aaron"&& nombre !="mensom" && nombre!="seba" && nombre!="daniela")
-    {let confirmar = confirm("No estás autorizado. ¿Deseas salir?");
-    if (confirmar) {
-        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
+    {location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"};
